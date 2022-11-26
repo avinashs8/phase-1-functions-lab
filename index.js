38c0e481ca04399c5c0f594be40491bdd9956871
avinash singh <avinash8.as@gmail.com>
2022-11-26 17:35:19 -0500

    commit tree 1d212e63b476b2850b40627645c00fb1c7581755

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    if (someValue > 42){
+        return someValue - 42
+    } else {
+        return 42- someValue
+    } 
+}
+function distanceFromHqInFeet (someValue){
+    return distanceFromHqInBlocks(someValue) * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (destination > start){
+        return (destination - start) * 264
+    } else 
+        return (start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination){
+    let price = distanceTravelledInFeet(start, destination)
+    if (price > 400 && price < 2000){
+        return (price -400) * .02
+    } else if (price > 2000 && price < 2500){
+        return 25
+    } else if (price >2500){
+        return 'cannot travel that far'
+    } else {
+        return 0
+    }
+}
